79b7ed013d4984ace1a2dcf7716d8d649b9b2e1c
annas <annas@DESKTOP-9GQU1I6>
2023-02-17 19:32:28 +0100

    La primera versión + JavaDoc

--- src/CCuenta.java
+++ src/CCuenta.java
@@ -1,7 +1,10 @@
 
 /**
- *
+ * Esta clase se utiliza para ingresar y retirar la cantidad de dinero de un cliente.
+ * 
  * @author Anna Slenduhhova
+ * @version 1.0.0
+ * @since 1.0.0
  */
 public class CCuenta {
 
@@ -11,10 +14,20 @@ public class CCuenta {
     private double saldo;
     private double tipoInterés;
 
+    /**
+     * Construcor sin parámetros
+     */
     public CCuenta()
     {
     }
-
+    
+    /**
+     * Constructor que inicializa los parametros de clase CCuenta 
+     * @param nom Nombre del cliente
+     * @param cue Número de la cuenta
+     * @param sal Saldo en la cuenta
+     * @param tipo Tipo de interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -22,11 +35,20 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Muestra el saldo de la cuenta
+     * @return devuelve el saldo de la cuenta
+     */
     public double estado()
     {
         return saldo;
     }
-
+    
+    /**
+     * Con este método se ingresa el dinero a la cuenta
+     * @param cantidad Cantidad de dinero a ingresar
+     * @throws Exception
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -34,6 +56,11 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Con este método se retira el dinero de la cuenta
+     * @param cantidad Cantidad de dinero a retirar
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
